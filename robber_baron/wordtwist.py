aa20de5c9255fb8614e2a7aaf488c8a49ab7a1f5
Avery Crespi <averycrespi@gmail.com>
2021-09-05 11:54:05 -0700

    Add logging

--- robber_baron/wordtwist.py
+++ robber_baron/wordtwist.py
@@ -13,19 +13,21 @@ BOARD_SIZE = 4  # 4 or 5
 if __name__ == "__main__":
     driver = webdriver.Chrome()
 
-    # Load the Start Game page and extract the board UID
-    driver.get(f"https://wordtwist.puzzlebaron.com/init{BOARD_SIZE}.php")
+    new_game_url = f"https://wordtwist.puzzlebaron.com/init{BOARD_SIZE}.php"
+    print(f"Loading new game URL: {new_game_url} ...")
+    driver.get(new_game_url)
     board_url = find_element(driver, "div#newgameboard a").get_attribute("href")
     board_uid = board_url.split("u=")[-1]
+    print(f"Extracted board UID: {board_uid}")
 
-    # We need to load the board URL _before_ we request the board data,
-    # otherwise WordTwist will complain that the game has already been completed
+    print(f"Loading board URL: {board_url} ...")
     driver.get(board_url)
     data_url = f"https://wordtwist.puzzlebaron.com/boarddata.php?uid={board_uid}"
+    print(f"Requesting board data from: {data_url} ...")
     board_data = json.loads(requests.get(data_url).text)
-    print(json.dumps(board_data, indent=4))
+    print(f"Parsed board data: {json.dumps(board_data)}")
 
-    # Start the game and submit all valid words
+    print("Starting game and entering words ...")
     find_element(driver, "a#start").click()
     words = list(board_data["wordList"].keys())
     word_input = find_element(driver, "input#word")
@@ -34,6 +36,7 @@ if __name__ == "__main__":
         word_input.send_keys(Keys.RETURN)
         time.sleep(0.1)
 
+    print("Submitting game ...")
     find_element(driver, "a#submit").click()
     find_element(driver, "div.TB_modal > div.alert a.yes").click()
 
